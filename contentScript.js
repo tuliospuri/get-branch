504b2f9a97f8f52de24c93e8e1f25b0c9060b61a
tuliospuri <tuliospuri@gmail.com>
2016-04-16 18:50:21 -0300

    feedback on icon after action

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,29 @@
 // Helper functions
-function getElementsByClassName(node,classname)
-{
-  if (node.getElementsByClassName) { // use native implementation if available
-    return node.getElementsByClassName(classname);
-  } else {
-    return (function getElementsByClass(searchClass,node) {
-        if ( node == null )
-          node = document;
-        var classElements = [],
-            els = node.getElementsByTagName("*"),
-            elsLen = els.length,
-            pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)"), i, j;
+function getElementsByClassName(node, classname) {
+    if (node.getElementsByClassName) { // use native implementation if available
+        return node.getElementsByClassName(classname);
+    } else {
+        return (function getElementsByClass(searchClass, node) {
+            if (node == null)
+                node = document;
+            var classElements = [],
+                els = node.getElementsByTagName("*"),
+                elsLen = els.length,
+                pattern = new RegExp("(^|\\s)" + searchClass + "(\\s|$)"),
+                i, j;
 
-        for (i = 0, j = 0; i < elsLen; i++) {
-          if ( pattern.test(els[i].className) ) {
-              classElements[j] = els[i];
-              j++;
-          }
-        }
-        return classElements;
-    })(classname, node);
-  }
+            for (i = 0, j = 0; i < elsLen; i++) {
+                if (pattern.test(els[i].className)) {
+                    classElements[j] = els[i];
+                    j++;
+                }
+            }
+            return classElements;
+        })(classname, node);
+    }
 }
 
-function copyToClipboard(text)
-{
+function copyToClipboard(text) {
     var copyDiv = document.createElement('div');
     copyDiv.contentEditable = true;
     document.body.appendChild(copyDiv);
@@ -40,17 +39,20 @@ function copyToClipboard(text)
 // End helper functions
 
 // Main
-function main()
-{
-  var title = getElementsByClassName(document, 'card-detail-title-assist')[0].innerText;
-  var prefix = title.match(/s\d+_c\d+/i);
-  var hasPrefix = prefix != undefined;
+function main() {
+    var title = getElementsByClassName(document, 'card-detail-title-assist')[0].innerText;
+    var prefix = title.match(/s\d+_c\d+/i);
+    var hasPrefix = prefix != undefined;
 
-  if (hasPrefix) {
-      return copyToClipboard('BCH_' + prefix[0]);
-  }
+    if (hasPrefix) {
+        var branch = 'BCH_' + prefix[0];
+        console.log('Get Branch: "' + branch + '" copiado!');
+        chrome.runtime.sendMessage({status: true});
 
-  return alert('Este cartão não tem um prefixo S000_C000 válido!');
+        return copyToClipboard(branch);
+    }
+
+    return alert('Este cartão não tem um prefixo S000_C000 válido!');
 }
 
 main();
--- eventPage.js
+++ eventPage.js
@@ -1,18 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {hostEquals: "trello.com", schemes: ["https"], pathPrefix: "/c/"},
-                        css: [".icon-card"]
-                    })
-                ],
-                actions: [
-                    new chrome.declarativeContent.ShowPageAction()
-                ]
-            }
-        ]);
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: "trello.com", schemes: ["https"], pathPrefix: "/c/"},
+                    css: [".icon-card"]
+                })
+            ],
+            actions: [
+                new chrome.declarativeContent.ShowPageAction()
+            ]
+        }]);
     });
     chrome.tabs.create({url: "installed.html"});
 });
@@ -20,3 +18,14 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, {file: "contentScript.js"});
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.status) {
+            chrome.pageAction.setIcon({tabId: sender.tab.id, path: 'icon_feedback_19.png'});
+            setTimeout(function() {
+                chrome.pageAction.setIcon({tabId: sender.tab.id, path: 'icon_19.png'});
+            }, 200);
+        }
+    }
+);
